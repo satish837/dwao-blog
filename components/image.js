2a85875442eb0b013c9a9bac523874dd210945ff
Shahid Khan <shahid.k@clik.ai>
2023-05-16 12:13:38 +0530

    added check in image module

--- components/image.js
+++ components/image.js
@@ -2,16 +2,19 @@ import { getStrapiMedia } from "../lib/media";
 import NextImage from "next/image";
 
 const Image = ({ image }) => {
-  const { alternativeText, width, height } = image.data.attributes;
-
-  return (
-    <NextImage
-      width={width}
-      height={height}
-      src={getStrapiMedia(image)}
-      alt={alternativeText || ""}
-    />
-  );
+  if (image.data && image.data.hasOwnProperty('attributes')) {
+    const { alternativeText, width, height } = image?.data?.attributes;
+    return (
+      <NextImage
+        width={width}
+        height={height}
+        src={getStrapiMedia(image)}
+        alt={alternativeText || ""}
+      />
+    );
+  } else {
+    return null;
+  }
 };
 
 export default Image;
