eb33fb26a6219105a7ab22800054b5005e12a12f
Shahid Khan <shahid.k@clik.ai>
2023-05-22 13:35:54 +0530

    rollback image code

--- components/image.js
+++ components/image.js
@@ -1,21 +1,17 @@
-import React from "react";
 import { getStrapiMedia } from "../lib/media";
 import NextImage from "next/image";
 
 const Image = ({ image }) => {
-  const imagePath = React.useMemo(() => image.data && image.data.hasOwnProperty('attributes') ? getStrapiMedia(image) : '', [image])
-  const [src, setSrc] = React.useState(imagePath);
   if (image.data && image.data.hasOwnProperty('attributes')) {
     const { alternativeText, width, height } = image?.data?.attributes;
     return (
       <NextImage
         width={width}
         height={height}
-        src={src}
+        src={getStrapiMedia(image)}
         alt={alternativeText || ""}
         priority={true}
-        blurDataURL={src}
-        onError={() => setSrc(src)}
+        blurDataURL={getStrapiMedia(image)}
       />
     );
   } else {
