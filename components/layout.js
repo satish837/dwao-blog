a8213fd1aae9688b8629feead4d41479b1ea6d77
Satish Kumar <kumar.satish837@gmail.com>
2023-10-16 19:23:36 +0530

    code update

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import Header from "./header";
 const Layout = ({ children, categories, seo }) => (
   <>
     <Header categories={categories} />
-    <div className="container" style={{paddingTop: '3rem'}}>
+    <div className="container articles" style={{paddingTop: '3rem'}}>
       <div className="mt-24 mb-12">
         {children}
       </div>
